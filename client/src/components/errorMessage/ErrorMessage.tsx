8174dfcffaa68d9156401497674a27d7337dff94
Nazarii <dzhaman.nazar2003@gmail.com>
2024-11-14 11:35:42 +0200

    implements RTK Query and update Login

---
+++ client/src/components/errorMessage/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import { Alert } from "antd";
+import { FC } from "react";
+
+interface ErrorMessageProps {
+	message?: string;
+}
+
+export const ErrorMessage: FC<ErrorMessageProps> = ({message}) => {
+	if(!message) {
+		return null;
+	}
+	return <Alert message={message} type="error"/>
+};
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,12 +1,33 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Layout } from "../../components/layout/Layout";
 import { Button, Card, Form, Row, Space, Typography } from "antd";
 import { Input } from "../../components/input/Input";
 import { PasswordInput } from "../../components/passwordInput/PasswordInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
+import { useLoginMutation, UserData } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/isErrorWithMessage";
+import { ErrorMessage } from "../../components/errorMessage/ErrorMessage";
 
 export const Login: FC = () => {
+	const navigate = useNavigate();
+	const [loginUser] = useLoginMutation();
+	const [error, setError] = useState("");
+
+	const onLoginForm = async (data: UserData) => {
+		try {
+			await loginUser(data).unwrap();
+			navigate("/");
+		} catch (error) {
+			const maybeError = isErrorWithMessage(error);
+			if (maybeError) {
+				setError(error.data.message);
+			} else {
+				setError("Something went wrong");
+			}
+		}
+	};
+
 	return (
 		<Layout>
 			<Row
@@ -15,7 +36,7 @@ export const Login: FC = () => {
 				<Card
 					title="Login form"
 					style={{ width: "30rem" }}>
-					<Form onFinish={() => console.log("Finish form")}>
+					<Form onFinish={onLoginForm}>
 						<Input
 							type="email"
 							name="email"
@@ -38,6 +59,7 @@ export const Login: FC = () => {
 							Don't have an account?{" "}
 							<Link to={Paths.register}>Register</Link>
 						</Typography.Text>
+						<ErrorMessage message={error} />
 					</Space>
 				</Card>
 			</Row>
